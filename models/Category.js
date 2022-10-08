e7ee729c69e66b4ccc9e37a236c422c67e6cb5c5
p5eudoDjnn <alexander.havers@gmail.com>
2022-10-08 15:07:00 -0400

    wrong branch

--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,19 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // define the id
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
