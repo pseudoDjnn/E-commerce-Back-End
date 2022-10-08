affb27eea6f20764f5906102aad48707b9e70fb1
p5eudoDjnn <alexander.havers@gmail.com>
2022-10-08 18:02:28 -0400

    added category model and ran server with no error

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,16 @@ class Category extends Model {}
 Category.init(
   {
     // define the id
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
