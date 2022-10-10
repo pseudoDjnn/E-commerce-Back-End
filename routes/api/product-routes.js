fcc1ae39a98a0816463caf7d42af9d104d996d7d
p5eudoDjnn <alexander.havers@gmail.com>
2022-10-10 17:04:32 -0400

    found problemand commented it out in product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -13,10 +13,10 @@ router.get("/", (req, res) => {
         model: Category,
         // attributes: ["id", "category_name"],
       },
-      {
-        model: Tag,
-        throguh: ProductTag,
-      },
+      // {
+      //   model: Tag,
+      //   through: ProductTag,
+      // },
     ],
   })
     .then((dbProductsData) => res.json(dbProductsData))
@@ -30,6 +30,31 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    include: [
+      {
+        model: Category,
+      },
+      // {
+      //   model: Tag,
+      //   through: ProductTag,
+      // },
+    ],
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbProductsData) => {
+      if (!dbProductsData) {
+        res.status(404).json({ message: "No product id found" });
+        return;
+      }
+      res.json(dbProductsData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // create new product
@@ -108,6 +133,15 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
+  ProductTag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((removeRes) => res.json(removeRes))
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
