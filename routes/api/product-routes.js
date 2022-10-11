c9548d732256ee91d918da4681ba39ce360ce409
p5eudoDjnn <alexander.havers@gmail.com>
2022-10-11 13:55:05 -0400

    updated the product POST and still throw an error once the server is up

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -59,15 +59,25 @@ router.get("/:id", (req, res) => {
 
 // create new product
 router.post("/", (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    categorry_id: req.body.category_id,
+    tagIds: [
+      {
+        include: [
+          {
+            model: Tag,
+            throught: ProductTag,
+            where: {
+              tasg_id: req.body.tag_id,
+            },
+          },
+        ],
+      },
+    ],
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
